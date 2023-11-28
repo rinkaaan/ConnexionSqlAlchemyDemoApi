bdfbe73d9ea312e7b8c571df6a55f0a3e609250b
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2023-11-28 05:09:47 -0800

    Use setup_app

--- src/app.py
+++ src/app.py
@@ -1,37 +1,11 @@
-import logging
 from pathlib import Path
 
-import connexion
-from connexion.middleware import MiddlewarePosition
-from flask.cli import load_dotenv
-from nguylinc_python_utils.sqlalchemy import init_db
-from starlette.middleware.cors import CORSMiddleware
+from nguylinc_python_utils.connexion import setup_app
 
 from src.init_models import Base
 
-load_dotenv()
-logging.basicConfig(level=logging.INFO)
-session = init_db(Base)
-
-origins = ["http://localhost:5173", "https://lincolnnguyen.me"]
-
-app = connexion.FlaskApp(__name__)
-app.add_api('openapi.yaml', validate_responses=True, strict_validation=True)
-app.add_middleware(
-    CORSMiddleware,
-    position=MiddlewarePosition.BEFORE_ROUTING,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-application = app.app
-
-
-@application.teardown_appcontext
-def shutdown_session(exception=None):
-    session.remove()
-
+schema_path = Path(__file__).parent / "openapi.yaml"
+app, session = setup_app(Base, schema_path)
 
 if __name__ == "__main__":
     app.run(f"{Path(__file__).stem}:app", port=8080, reload=True)
